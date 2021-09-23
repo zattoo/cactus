d0fc317155fc38daa3eea10c4feb825ef3245aea
kim <kimngan.ledang@zattoo.com>
2021-09-23 12:08:42 +0200

    console.log

--- src/index.js
+++ src/index.js
@@ -136,6 +136,7 @@ const getNewVersions = (changelogBefore, changelogAfter) => {
                 const changelogPath = `projects/${project}/CHANGELOG.md`;
                 const changelog = await fse.readFile(changelogPath, 'utf-8');
                 const lastVersion = getLastVersion(version);
+                console.log(lastVersion);
                 const split = `## [${lastVersion}]`;
                 const [before, after] = changelog.split(split);
                 const newEntry = `## ${item.title}\n\n${item.body}\n\n`;
