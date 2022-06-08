2a7d4bb341b9e0361a345bd04dc9c74b93ba1202
Max Kieltyka <max.kieltyka@zattoo.com>
2022-06-08 13:16:27 +0200

    test setup

--- src/index.js
+++ src/index.js
@@ -1,179 +1,181 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const parseChangelog = require('changelog-parser');
+// const core = require('@actions/core');
+// const github = require('@actions/github');
+// const parseChangelog = require('changelog-parser');
 
-let foundSomething = false;
+// let foundSomething = false;
 
-const isEmpty = (value) => {
-    return (
-        value === undefined ||
-        value === null ||
-        (typeof value === 'object' && Object.keys(value).length === 0) ||
-        (typeof value === 'string' && value.trim().length === 0)
-    );
-};
+// const isEmpty = (value) => {
+//     return (
+//         value === undefined ||
+//         value === null ||
+//         (typeof value === 'object' && Object.keys(value).length === 0) ||
+//         (typeof value === 'string' && value.trim().length === 0)
+//     );
+// };
 
-const exit = (message, exitCode) => {
-    if (exitCode === 1) {
-        core.error(message);
-    } else {
-        core.info(message);
-    }
+// const exit = (message, exitCode) => {
+//     if (exitCode === 1) {
+//         core.error(message);
+//     } else {
+//         core.info(message);
+//     }
 
-    process.exit(exitCode);
-};
+//     process.exit(exitCode);
+// };
 
-const getNewVersions = (project, changelogBefore, changelogAfter) => {
-    let newVersions = [];
+// const getNewVersions = (project, changelogBefore, changelogAfter) => {
+//     let newVersions = [];
 
-    const mapBefore = changelogBefore.versions.reduce((result, item) => {
-        return {
-            ...result,
-            [item.version]: item,
-        };
-    }, {});
+//     const mapBefore = changelogBefore.versions.reduce((result, item) => {
+//         return {
+//             ...result,
+//             [item.version]: item,
+//         };
+//     }, {});
 
-    changelogAfter.versions.forEach((item) => {
-        const versionAfter = item.version;
-        const dateAfter = item.date;
-        const itemBefore = mapBefore[versionAfter] || {};
-        const dateBefore = itemBefore.date;
+//     changelogAfter.versions.forEach((item) => {
+//         const versionAfter = item.version;
+//         const dateAfter = item.date;
+//         const itemBefore = mapBefore[versionAfter] || {};
+//         const dateBefore = itemBefore.date;
 
-        if (!dateBefore && dateAfter) {
-            core.info(`New ${versionAfter}-${project} version detected, preparing candidate...`);
-            foundSomething = true;
-            newVersions.push(item);
-        }
-    });
+//         if (!dateBefore && dateAfter) {
+//             core.info(`New ${versionAfter}-${project} version detected, preparing candidate...`);
+//             foundSomething = true;
+//             newVersions.push(item);
+//         }
+//     });
 
-    return newVersions;
-};
+//     return newVersions;
+// };
 
-(async () => {
-    const token = core.getInput('token', {required: true});
-    const labels = core.getMultilineInput('labels', {required: false});
-    const octokit = github.getOctokit(token);
+// (async () => {
+//     const token = core.getInput('token', {required: true});
+//     const labels = core.getMultilineInput('labels', {required: false});
+//     const octokit = github.getOctokit(token);
 
-    const {context} = github;
-    const {payload} = context;
+//     const {context} = github;
+//     const {payload} = context;
 
-    const {
-        after,
-        before,
-        repository,
-    } = payload;
+//     const {
+//         after,
+//         before,
+//         repository,
+//     } = payload;
 
-    const repo = repository.name;
-    const owner = repository.full_name.split('/')[0];
+//     const repo = repository.name;
+//     const owner = repository.full_name.split('/')[0];
 
-    const commit = await octokit.rest.repos.getCommit({
-        owner,
-        repo,
-        ref: after,
-    });
+//     const commit = await octokit.rest.repos.getCommit({
+//         owner,
+//         repo,
+//         ref: after,
+//     });
 
-    const {files} = commit.data;
+//     const {files} = commit.data;
 
-    if (isEmpty(files)) {
-        exit('No changes', 0);
-    }
+//     if (isEmpty(files)) {
+//         exit('No changes', 0);
+//     }
 
-    const changelogs = files.filter((file) => file.filename.includes('CHANGELOG.md'));
+//     const changelogs = files.filter((file) => file.filename.includes('CHANGELOG.md'));
 
-    if (isEmpty(changelogs)) {
-        exit('No changelog changes', 0);
-    }
+//     if (isEmpty(changelogs)) {
+//         exit('No changelog changes', 0);
+//     }
 
-    const cut = async (project, item) => {
-        const {version} = item;
-        const release = version.slice(0, -2);
-        const first = Number(version[version.length - 1]) === 0;
+//     const cut = async (project, item) => {
+//         const {version} = item;
+//         const release = version.slice(0, -2);
+//         const first = Number(version[version.length - 1]) === 0;
 
-        if (!first) {
-            exit(`This is not a first change to ${release} release`, 0);
-        }
+//         if (!first) {
+//             exit(`This is not a first change to ${release} release`, 0);
+//         }
 
-        const rcBranch = `rc/${project}/${version}`;
-        const releaseBranch = `release/${project}/${release}`;
+//         const rcBranch = `rc/${project}/${version}`;
+//         const releaseBranch = `release/${project}/${release}`;
 
-        await Promise.all([
-            await octokit.rest.git.createRef({
-                owner,
-                repo,
-                ref: `refs/heads/${releaseBranch}`,
-                sha: before,
-            }),
-            await octokit.rest.git.createRef({
-                owner,
-                repo,
-                ref: `refs/heads/${rcBranch}`,
-                sha: after,
-            }),
-        ]);
+//         await Promise.all([
+//             await octokit.rest.git.createRef({
+//                 owner,
+//                 repo,
+//                 ref: `refs/heads/${releaseBranch}`,
+//                 sha: before,
+//             }),
+//             await octokit.rest.git.createRef({
+//                 owner,
+//                 repo,
+//                 ref: `refs/heads/${rcBranch}`,
+//                 sha: after,
+//             }),
+//         ]);
 
-        const body = `## Changelog\n\n${item.body}\n\n`;
+//         const body = `## Changelog\n\n${item.body}\n\n`;
 
-        const {data: pr} = await octokit.rest.pulls.create({
-            owner,
-            repo,
-            title: `Release ${version}-${project}`,
-            body,
-            head: rcBranch,
-            base: releaseBranch,
-        })
+//         const {data: pr} = await octokit.rest.pulls.create({
+//             owner,
+//             repo,
+//             title: `Release ${version}-${project}`,
+//             body,
+//             head: rcBranch,
+//             base: releaseBranch,
+//         })
 
-        await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr.number,
-            labels,
-        });
-    };
+//         await octokit.rest.issues.addLabels({
+//             owner,
+//             repo,
+//             issue_number: pr.number,
+//             labels,
+//         });
+//     };
 
-    const processChanges = async (item) => {
-        const {filename} = item;
+//     const processChanges = async (item) => {
+//         const {filename} = item;
 
-        const split = filename.split('/');
-        const project = split[split.length - 2];
+//         const split = filename.split('/');
+//         const project = split[split.length - 2];
 
-        core.info(`Analyzing ${project} project...`);
+//         core.info(`Analyzing ${project} project...`);
 
-        const [contentBefore, contentAfter] = await Promise.all([
-            await octokit.rest.repos.getContent({
-                owner,
-                repo,
-                path: filename,
-                ref: before,
-            }),
-            await octokit.rest.repos.getContent({
-                owner,
-                repo,
-                path: filename,
-                ref: after,
-            }),
-        ]);
+//         const [contentBefore, contentAfter] = await Promise.all([
+//             await octokit.rest.repos.getContent({
+//                 owner,
+//                 repo,
+//                 path: filename,
+//                 ref: before,
+//             }),
+//             await octokit.rest.repos.getContent({
+//                 owner,
+//                 repo,
+//                 path: filename,
+//                 ref: after,
+//             }),
+//         ]);
 
-        const textBefore = Buffer.from(contentBefore.data.content, 'base64').toString();
-        const textAfter = Buffer.from(contentAfter.data.content, 'base64').toString();
+//         const textBefore = Buffer.from(contentBefore.data.content, 'base64').toString();
+//         const textAfter = Buffer.from(contentAfter.data.content, 'base64').toString();
 
-        const [changelogBefore, changelogAfter] = await Promise.all([
-            await parseChangelog({text: textBefore}),
-            await parseChangelog({text: textAfter}),
-        ]);
+//         const [changelogBefore, changelogAfter] = await Promise.all([
+//             await parseChangelog({text: textBefore}),
+//             await parseChangelog({text: textAfter}),
+//         ]);
 
-        const newVersions = getNewVersions(project, changelogBefore, changelogAfter);
+//         const newVersions = getNewVersions(project, changelogBefore, changelogAfter);
 
-        if (!isEmpty(newVersions)) {
-            await Promise.all(newVersions.map((version) => cut(project, version)));
-        }
-    };
+//         if (!isEmpty(newVersions)) {
+//             await Promise.all(newVersions.map((version) => cut(project, version)));
+//         }
+//     };
 
-    await Promise.all(changelogs.map(processChanges));
+//     await Promise.all(changelogs.map(processChanges));
 
-    if (!foundSomething) {
-        exit('No release candidates were found', 0);
-    }
-})()
-    .catch((error) => {
-        exit(error, 1);
-    });
+//     if (!foundSomething) {
+//         exit('No release candidates were found', 0);
+//     }
+// })()
+//     .catch((error) => {
+//         exit(error, 1);
+//     });
+
+exit('test setup', 1);
