70d16caac952a897f1d8c4384c6094e76aa5d5e5
Max Kieltyka <max.kieltyka@zattoo.com>
2022-06-28 16:00:08 +0200

    add error

---
+++ src/error.js
@@ -0,0 +1,20 @@
+import * as core from '@actions/core';
+
+export class GithubError extends Error {
+    constructor(message, apiError) {
+
+        core.info({
+            apiError: JSON.stringify(apiError, Object.getOwnPropertyNames(apiError)),
+            message: apiError.message,
+        });
+
+        const {message: apiMessage} = JSON.parse(apiError.message);
+
+        super(`${message}: ${apiMessage}`);
+        this.name = 'GithubError';
+    }
+}
+
+export const isGithubError = (error) => {
+    return error instanceof GithubError;
+}
