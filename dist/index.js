d1832f7636bd0ccfceb45dfefb73283c32e6cc13
Max Kieltyka <max.kieltyka@zattoo.com>
2022-06-28 18:14:54 +0200

    test

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 7351:
@@ -32485,4 +32485,3 @@ const createReleaseCandidatePullRequest = async ({
 module.exports = __webpack_exports__;
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
